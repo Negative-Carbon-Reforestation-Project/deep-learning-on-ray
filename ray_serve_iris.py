8517ae1f8d0de1b8cdd1461e4e71d00efbc5697a
liamstar97 <liamstar@gmail.com>
2022-06-07 12:54:00 -0700

    created new test job to serve on ray

---
+++ ray_serve_iris.py
@@ -0,0 +1,32 @@
+import requests
+
+from sklearn.datasets import load_iris
+from sklearn.ensemble import GradientBoostingClassifier
+
+from ray import serve
+
+serve.start()
+
+# Train model.
+iris_dataset = load_iris()
+model = GradientBoostingClassifier()
+model.fit(iris_dataset["data"], iris_dataset["target"])
+
+
+@serve.deployment(route_prefix="/iris")
+class BoostingModel:
+    def __init__(self, model):
+        self.model = model
+        self.label_list = iris_dataset["target_names"].tolist()
+
+    async def __call__(self, request):
+        payload = await request.json()
+        print(f"Received flask request with data {payload}")
+
+        prediction = self.model.predict([payload["vector"]])[0]
+        human_name = self.label_list[prediction]
+        return {"result": human_name}
+
+
+# Deploy model.
+BoostingModel.deploy(model)
