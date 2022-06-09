f1b32258a601102a5fa805fdaf68bb9def2bf242
liamstar97 <liamstar@gmail.com>
2022-06-09 11:12:28 -0700

    adding pretrained model to be downloaded to ray cluster

--- ray_serve_tensorflow_with_keras.py
+++ ray_serve_tensorflow_with_keras.py
@@ -5,48 +5,13 @@ import os
 import time
 import numpy as np
 
-print(os.getcwd())
-
-
-def train_and_save_model():
-    import tensorflow as tf
-
-    # Load mnist dataset
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    # Train a simple neural net model
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Flatten(input_shape=(28, 28)),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(10),
-        ]
-    )
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
-    model.fit(x_train, y_train, epochs=1)
-
-    model.evaluate(x_test, y_test, verbose=2)
-    model.summary()
-
-    # Save the model in h5 format in local file system
-    # model.save(TRAINED_MODEL_PATH)
-
-
-# if not os.path.exists(TRAINED_MODEL_PATH):
-#     train_and_save_model()
-
-
 @serve.deployment(route_prefix="/mnist")
 class TFMnistModel:
-    def __init__(self):
+    def __init__(self, model_path):
         import tensorflow as tf
 
-        # self.model_path = model_path
-        # self.model = tf.keras.models.load_model(model_path)
+        self.model_path = model_path
+        self.model = tf.keras.models.load_model(model_path)
 
     async def __call__(self, starlette_request):
         # Step 1: transform HTTP request -> tensorflow input
@@ -63,7 +28,7 @@ class TFMnistModel:
 
 ray.init('ray://ray-ray-head:10001')
 serve.start()
-TFMnistModel.deploy()
+TFMnistModel.deploy('./models/mnist_model.h5')
 
 while True:
     time.sleep(5)
