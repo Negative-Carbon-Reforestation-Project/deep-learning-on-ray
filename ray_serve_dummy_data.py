bdc00e2fd2553a915fafd4b861ca94507f2198c9
liamstar97 <liamstar@gmail.com>
2022-06-11 16:25:55 -0700

    adding dummy API and gitIgnore changes

---
+++ ray_serve_dummy_data.py
@@ -0,0 +1,23 @@
+import ray
+import time
+
+from ray import serve
+
+
+@serve.deployment(route_prefix="/opportunity")
+class ReforestationModel:
+    def __init__(self):
+        import time
+
+    async def __call__(self, starlette_request):
+        print(starlette_request)
+        return {"prediction": 69}
+
+
+ray.init('ray://ray-ray-head:10001')
+serve.start()
+ReforestationModel.deploy()
+
+while True:
+    time.sleep(5)
+    print(serve.list_deployments())
